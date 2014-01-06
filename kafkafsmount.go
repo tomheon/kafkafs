835f076b8aa1a325a4d30dcabe9477182dc3e53d
Edmund Jorgensen <tomheon@gmail.com>
2014-01-06 14:33:45 -0500

    Make our own FlagSet to get rid of -test flag pollution.

--- kafkafsmount.go
+++ kafkafsmount.go
@@ -17,15 +17,6 @@ import (
 	"github.com/tomheon/kafkafs/kafkafs"
 )
 
-var kafkaAddrs = flag.String("kafkaAddrs", "localhost:9092",
-	"Kafka server addresses host1:port1[;host2:port2...]")
-var metadataRetries = flag.Int("metadataRetries", 10,
-	"Max times to attempt metadata refresh from Kafka before failing")
-var waitForElectionMs = flag.Int("waitForElectionMs", 250,
-	"Max milliseconds to wait for Kafka leader election before failing")
-var maxMsgBytes = flag.Int("maxMsgBytes", 1024*1024*10,
-	"Max bytes to pull for a single message")
-
 func unmountOnInt(c chan os.Signal, server *fuse.Server, client *sarama.Client) {
 	s := <-c
 	fmt.Println("Got signal:", s)
@@ -34,19 +25,30 @@ func unmountOnInt(c chan os.Signal, server *fuse.Server, client *sarama.Client) 
 }
 
 func main() {
-	flag.Parse()
 
-	flag.Usage = func() {
+	var commandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	var kafkaAddrs = commandLine.String("kafkaAddrs", "localhost:9092",
+		"Kafka server addresses host1:port1[;host2:port2...]")
+	var metadataRetries = commandLine.Int("metadataRetries", 10,
+		"Max times to attempt metadata refresh from Kafka before failing")
+	var waitForElectionMs = commandLine.Int("waitForElectionMs", 250,
+		"Max milliseconds to wait for Kafka leader election before failing")
+	var maxMsgBytes = commandLine.Int("maxMsgBytes", 1024*1024*10,
+		"Max bytes to pull for a single message")
+
+	commandLine.Parse(os.Args[1:])
+
+	commandLine.Usage = func() {
 		fmt.Fprintf(os.Stderr, "usage: kafkafs [options] mountpoint\n")
-		flag.PrintDefaults()
+		commandLine.PrintDefaults()
 		os.Exit(2)
 	}
 
-	if flag.NArg() != 1 {
-		flag.Usage()
+	if commandLine.NArg() != 1 {
+		commandLine.Usage()
 	}
 
-	mountpoint := flag.Arg(0)
+	mountpoint := commandLine.Arg(0)
 
 	addrs := strings.Split(*kafkaAddrs, ";")
 	client, err := sarama.NewClient("kafkafs", addrs,
